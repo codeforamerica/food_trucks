fb80cffc097a8873004a305c35e2d7320aaf8d7d
Ryan Closner <ryan@ryanclosner.com>
2013-08-19 12:22:56 -0700

    Allow time slots to be sorted on the location page

--- app/admin/locations.rb
+++ app/admin/locations.rb
@@ -10,18 +10,20 @@ ActiveAdmin.register Location do
 
   show do
     panel "Time Slots" do
-      table_for(location.time_slots) do |t|
-        t.column :start_at
-        t.column :finish_at
-        t.column :vendor
-        t.column(:availabity) do |time_slot|
-          status_tag(time_slot.available? ? "Open" : "Reserved")
-        end
 
+      order = ""
+      if params[:order]
+        order = [ params[:order].gsub(/_(asc|desc)/, ''), params[:order].include?('asc') ? 'asc' : 'desc' ].join(" ")
+      end
+
+      table_for(vendor.time_slots.order(order), sortable: true) do |t|
+        t.column :start_at, sortable: :start_at
+        t.column :finish_at, sortable: :finish_at
+        t.column :vendor, sortable: :vendor_id
         t.column(:actions) do |time_slot|
-          [ link_to("View", admin_location_time_slot_path(location_id: location.id, id: time_slot)), 
-            link_to("Edit", edit_admin_location_time_slot_path(location_id: location.id, id: time_slot)), 
-            link_to("Delete", admin_location_time_slot_path(location_id: location.id, id: time_slot.id), method: :delete) ].join(", ").html_safe
+          [ link_to("View", admin_vendor_time_slot_path(vendor_id: vendor.id, id: time_slot)), 
+            link_to("Edit", edit_admin_vendor_time_slot_path(vendor_id: vendor.id, id: time_slot)), 
+            link_to("Delete", admin_vendor_time_slot_path(vendor_id: vendor.id, id: time_slot.id), method: :delete) ].join(", ").html_safe
         end
 
       end
