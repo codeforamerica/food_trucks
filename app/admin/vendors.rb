327e5920ae6236916a081e2f8380f176c0154747
Ryan Closner <ryan@ryanclosner.com>
2013-08-19 12:15:02 -0700

    Allow timeslots to be sorted within the vendor show page

--- app/admin/vendors.rb
+++ app/admin/vendors.rb
@@ -16,14 +16,16 @@ ActiveAdmin.register Vendor do
 
   show do
     panel "Time Slots" do
-      table_for(vendor.time_slots) do |t|
-        t.column :start_at
-        t.column :finish_at
-        t.column :location
-        t.column(:availabity) do |time_slot|
-          status_tag(time_slot.available? ? "Open" : "Reserved")
-        end
 
+      order = ""
+      if params[:order]
+        order = [ params[:order].gsub(/_(asc|desc)/, ''), params[:order].include?('asc') ? 'asc' : 'desc' ].join(" ")
+      end
+
+      table_for(vendor.time_slots.order(order), sortable: true) do |t|
+        t.column :start_at, sortable: :start_at
+        t.column :finish_at, sortable: :finish_at
+        t.column :location, sortable: :location_id
         t.column(:actions) do |time_slot|
           [ link_to("View", admin_vendor_time_slot_path(vendor_id: vendor.id, id: time_slot)), 
             link_to("Edit", edit_admin_vendor_time_slot_path(vendor_id: vendor.id, id: time_slot)), 
