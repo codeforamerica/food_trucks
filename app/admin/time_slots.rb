d4151c4b02529d93b9ff4bb2422e7dd8d89a5a07
Ryan Closner <ryan@ryanclosner.com>
2013-10-04 13:23:08 +0200

    Fixes #10 - Time Slot commenting error

--- app/admin/time_slots.rb
+++ app/admin/time_slots.rb
@@ -4,6 +4,25 @@ ActiveAdmin.register TimeSlot do
   controller.send(:belongs_to, :location, :vendor, polymorphic: true)
 
   controller do
+    def show
+      if params.include?(:location_id) || params.include?(:vendor_id)
+        super
+      else
+        @time_slot = TimeSlot.find(params[:id])
+        if @time_slot.location_id
+          redirect_to admin_location_time_slot_path(
+            @time_slot.attributes.extract!("location_id", "id")
+          )
+        elsif @time_slot.vendor_id
+          redirect_to admin_vendor_time_slot_path(
+            @time_slot.attributes.extract!("vendor_id", "id")
+          )
+        else
+          raise "Invalid TimeSlot"
+        end
+      end
+    end
+
     def destroy
       @time_slot = TimeSlot.find(params[:id])
       @time_slot.destroy
